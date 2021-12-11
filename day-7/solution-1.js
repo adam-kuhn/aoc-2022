ea365d313ac69389beaeffff36b7fe3c40ef6910
ada-kuhn <adam.kuhn20@gmail.com>
2021-12-11 22:32:58 +0200

    day 7 part one

---
+++ day-7/solution-1.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const testInput = fs.readFileSync('./day-7/day7-1_input.txt', 'utf-8')
+  .split(',').map(i => Number(i))
+
+const max = Math.max(...testInput)
+const min = Math.min(...testInput)
+
+let minFuel
+
+for (let pos = min; pos <= max; pos++) {
+  let fuelUsed = 0
+  for (let crab of testInput) {
+    fuelUsed += Math.abs(crab - pos)
+    if (minFuel && fuelUsed >= minFuel) break
+  }
+  if (fuelUsed < minFuel || !minFuel) {
+    minFuel = fuelUsed
+  }
+}
+
+console.log(minFuel)
